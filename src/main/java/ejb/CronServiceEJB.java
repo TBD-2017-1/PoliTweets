a0c3a25d24b593a7d79e5701e0b2824d1ac87efe
MonodetH <monodeth@gmail.com>
2017-05-15 02:36:21 -0400

    Add Cron Bean

--- src/main/java/ejb/CronServiceEJB.java
+++ src/main/java/ejb/CronServiceEJB.java
@@ -3,11 +3,18 @@ package ejb;
 import PoliTweetsCL.Core.BD.MongoDBController;
 import PoliTweetsCL.Core.Model.Tweet;
 import PoliTweetsCL.TextAPI.TextIndex;
+import model.Conglomerado;
+import model.Keyword;
+import model.Partido;
+import model.Politico;
 
+import javax.ejb.EJB;
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
 
 @Startup
 @Singleton
@@ -18,7 +25,14 @@ public class CronServiceEJB {
     @Inject
     private TextIndex textIndex;
 
-    //@Schedule(persistent=false) // Todos los dias a las 00:00
+    @EJB
+    private ConglomeradoFacadeEJB conglomeradoEJB;
+    @EJB
+    private PartidoFacadeEJB partidoEJB;
+    @EJB
+    private PoliticoFacadeEJB politicoEJB;
+
+    @Schedule(persistent=false) // Todos los dias a las 00:00
     public void doCRON(){
         doIndexation();
         doMetricasPoliticos();
@@ -35,62 +49,92 @@ public class CronServiceEJB {
     }
 
     private void doMetricasPoliticos(){
-        // para cada politico
-            // seleccionar las keyword de politico,
-            String[] keywords = new String[0];
+        List<Politico> politicos = politicoEJB.findAll();
+        // para cada Politico
+        for (Politico politico:politicos) {
+            // seleccionar las keyword de politico
+            List<Keyword> keywords = politico.getKeywords();
+            List<String> kwArray = new ArrayList<>();
+            for (Keyword word:keywords) {
+                kwArray.add(word.getValue());
+            }
+            if(politico.getCuentaTwitter()!=null){
+                kwArray.add(politico.getCuentaTwitter());
+            }
 
             // hacer busqueda
-            int hits = textIndex.buscarKeywords(keywords);
+            int hits = textIndex.buscarKeywords(kwArray.toArray(new String[0]));
 
             // obtener resultados de la busqueda anterior
             int positiveCount = textIndex.getPositiveCount();
             int negativeCount = textIndex.getNegativeCount();
             int neutralCount = textIndex.getNeutralCount();
+            float aprobacion = 50 + 50 * (positiveCount-negativeCount)/(float)hits; // 50% base + (%pos - %neg)/2
 
             // guardar metrica en BD
-        // fin foreach
+            /* IMPLEMENTAR *///////////////////////////////////////////////////////////////////////////////////////////////////////
 
+
+        }
     }
 
     private void doMetricasPartidos(){
-        // para cada politico
+        List<Partido> partidos = partidoEJB.findAll();
+        // para cada partido
+        for (Partido partido:partidos) {
             // seleccionar las keyword de partido,
-            String[] keywords = new String[0];
+            List<Keyword> keywords = partido.getKeywords();
+            List<String> kwArray = new ArrayList<>();
+            for (Keyword word:keywords) {
+                kwArray.add(word.getValue());
+            }
+            if(partido.getCuentaTwitter()!=null){
+                kwArray.add(partido.getCuentaTwitter());
+            }
 
             // hacer busqueda
-            int hits = textIndex.buscarKeywords(keywords);
+            int hits = textIndex.buscarKeywords(kwArray.toArray(new String[0]));
 
             // obtener resultados de la busqueda anterior
             int positiveCount = textIndex.getPositiveCount();
             int negativeCount = textIndex.getNegativeCount();
             int neutralCount = textIndex.getNeutralCount();
-
-            // Sumar metricas de politicos
-            positiveCount = positiveCount + (int)(0.5*positiveCount); // Asi las menciones directas al partido valen el doble que las heredadas de politicos
+            float aprobacion = 50 + 50 * (positiveCount-negativeCount)/(float)hits; // 50% base + (%pos - %neg)/2
 
             // guardar metrica en BD
-            // fin foreach
+
+
+        }
     }
 
     private void doMetricasConglomerados(){
+        List<Conglomerado> conglomerados = conglomeradoEJB.findAll();
         // para cada Conglomerado
+        for (Conglomerado congl:conglomerados) {
             // seleccionar las keyword de conglomerado,
-            String[] keywords = new String[0];
+            List<Keyword> keywords = congl.getKeywords();
+            List<String> kwArray = new ArrayList<>();
+            for (Keyword word:keywords) {
+                kwArray.add(word.getValue());
+            }
+            if(congl.getCuentaTwitter()!=null){
+                kwArray.add(congl.getCuentaTwitter());
+            }
 
             // hacer busqueda
-            int hits = textIndex.buscarKeywords(keywords);
+            int hits = textIndex.buscarKeywords(kwArray.toArray(new String[0]));
 
             // obtener resultados de la busqueda anterior
             int positiveCount = textIndex.getPositiveCount();
             int negativeCount = textIndex.getNegativeCount();
             int neutralCount = textIndex.getNeutralCount();
-
-            // Sumar metricas de partidos
-            positiveCount = positiveCount + (int)(0.5*positiveCount); // Asi las menciones directas al partido valen el doble que las heredadas de partidos
-            negativeCount = negativeCount + (int)(0.5*negativeCount);
+            float aprobacion = 50 + 50 * (positiveCount-negativeCount)/(float)hits; // 50% base + (%pos - %neg)/2
 
             // guardar metrica en BD
-        // fin foreach
+
+
+        }
+
     }
 
 
